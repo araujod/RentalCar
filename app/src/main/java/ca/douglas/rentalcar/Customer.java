e55b207a9da7d59dd8e1d6bf4d0991797e540604
300108357 <skdfjkdj>
2019-06-26 12:27:50 -0700

    Adding database of customer

---
+++ app/src/main/java/ca/douglas/rentalcar/Customer.java
@@ -0,0 +1,13 @@
+package ca.douglas.rentalcar;
+
+import java.io.Serializable;
+
+public class Customer implements Serializable {
+
+    String id;
+    String customerName;
+    String eamil;
+    String phone;
+    String gender;
+
+}
