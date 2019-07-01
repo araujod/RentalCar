14ddfd0faf5eb324bde1a2ca4a40027298d94c5c
David Araujo <david.daraujo@gmail.com>
2019-06-30 18:59:57 -0700

    Add new car GUI rework

--- app/src/main/java/ca/douglas/rentalcar/manager/AddNewCar.java
+++ app/src/main/java/ca/douglas/rentalcar/manager/AddNewCar.java
@@ -2,6 +2,7 @@ package ca.douglas.rentalcar.manager;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
 
 import ca.douglas.rentalcar.R;
 
