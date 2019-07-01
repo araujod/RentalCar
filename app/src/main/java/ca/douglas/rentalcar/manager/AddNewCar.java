aa1eab817995811fc79baab3d55fd49320744f71
Andre Sa <andreg_sa@hotmail.com>
2019-06-30 19:35:03 -0700

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit 96d9b58a93f4b27c460db6ded048787a4279e1de.

--- app/src/main/java/ca/douglas/rentalcar/manager/AddNewCar.java
+++ app/src/main/java/ca/douglas/rentalcar/manager/AddNewCar.java
@@ -2,7 +2,6 @@ package ca.douglas.rentalcar.manager;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.design.widget.TextInputLayout;
 
 import ca.douglas.rentalcar.R;
 
